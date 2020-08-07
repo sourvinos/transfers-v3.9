c04aafac96d8b29726a31c290e7dfd7073f3e073
John Sourvinos <johnsourvinos@hotmail.com>
2020-08-07 18:09:28 +0300

    Adds logo in home page

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { Title } from '@angular/platform-browser'
 import { HelperService } from '../shared/services/helper.service'
+import { environment } from 'src/environments/environment'
 
 @Component({
     selector: 'app-home',
@@ -13,6 +14,11 @@ export class HomeComponent implements OnInit {
     windowTitle = 'Home'
 
     constructor(private helperService: HelperService, private titleService: Title) { }
+
+    appName = {
+        header: environment.appName.header,
+        subHeader: environment.appName.subHeader
+    }
 
     ngOnInit() {
         this.setWindowTitle()
--- ClientApp/src/environments/environment.ts
+++ ClientApp/src/environments/environment.ts
@@ -3,5 +3,9 @@ export const environment = {
     login: {
         userName: 'sourvinos',
         password: '46929e6c-ee70-447a-ba35-542b4be14746'
+    },
+    appName: {
+        header: 'Island',
+        subHeader: 'Cruises'
     }
 }
